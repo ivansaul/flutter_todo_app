dfb1b0aa74bfb7db0a81c8c6130c096c92740293
ivansaul <saul_2503@hotmail.com>
2023-05-26 18:18:50 -0500

    update todo entitie

--- lib/entities/todo.dart
+++ lib/entities/todo.dart
@@ -1,31 +1,17 @@
-import 'package:hive/hive.dart';
+import 'package:isar/isar.dart';
+
 part "todo.g.dart";
 
-@HiveType(typeId: 1)
+@collection
 class Todo {
-  @HiveField(0)
+  Id isarId = Isar.autoIncrement;
   final String id;
-  @HiveField(1)
-  final String description;
-  @HiveField(2)
-  final bool completed;
+  String description;
+  bool completed;
 
   Todo({
     required this.id,
     required this.description,
     required this.completed,
   });
-
-  Todo copyWith({
-    String? id,
-    String? description,
-    bool? completed,
-    // String? title,
-    // String? subtitle,
-  }) =>
-      Todo(
-        id: id ?? this.id,
-        description: description ?? this.description,
-        completed: completed ?? this.completed,
-      );
 }
